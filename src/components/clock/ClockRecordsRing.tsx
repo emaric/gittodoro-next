0e410f5619f15d802836c39d61592e412ba1dd0b
E. Maric <emaric.dev@gmail.com>
2022-05-05 10:31:30 +0800

    :lipstick: memoize ClockRecordsRing

--- src/components/clock/ClockRecordsRing.tsx
+++ src/components/clock/ClockRecordsRing.tsx
@@ -1,11 +1,9 @@
-import { memo, useMemo } from "react"
+import { memo } from "react"
 
-import * as DateTime from "@/modules/temporal/DateTime"
 import { Clock } from "@/models/Clock"
-import { Record, filterRecords as filterRecordsUtil } from "@/models/Record"
+import { Record } from "@/models/Record"
 
 import ClockRing from "./ClockRing"
-
 
 interface Props {
   clock: Clock
@@ -13,22 +11,9 @@ interface Props {
 }
 
 const ClockRecordsRing = ({ clock, records }: Props) => {
-  const filteredRecords = useMemo(() => {
-    const filteredRecords = filterRecordsUtil(clock, records)
-    return filteredRecords.map((record) => {
-      record.start = DateTime.difference(clock.start, record.start) > 0
-        ? clock.start
-        : record.start
-      record.end = DateTime.difference(record.end, DateTime.now()) > 0
-        ? DateTime.now()
-        : record.end
-      return record
-    })
-  }, [clock, records])
-
   return (
     <>
-      {filteredRecords.map((record, i) => (
+      {records.map((record, i) => (
         <ClockRing key={i}
           state={record.state}
           start={record.start}
@@ -39,4 +24,6 @@ const ClockRecordsRing = ({ clock, records }: Props) => {
   )
 }
 
-export default memo(ClockRecordsRing)
+export default memo(ClockRecordsRing, (prev, next) => {
+  return JSON.stringify(prev) == JSON.stringify(next)
+})
