0ffb3736fbb621c360a44be9e0d29c6dbf1f3e38
E. Maric <emaric.dev@gmail.com>
2022-05-09 13:33:42 +0800

    :lipstick: created basic Markdown style

--- src/components/notes/NoteContent.tsx
+++ src/components/notes/NoteContent.tsx
@@ -1,10 +1,16 @@
 import { Markdown } from '@/modules/markdown'
 
+import styles from './Markdown.module.css'
+
 interface Props {
   children: string
   onChange: (content: string) => void
 }
 
 export const NoteContent = ({ children, onChange }: Props) => {
-  return <Markdown onChange={onChange}>{children}</Markdown>
+  return (
+    <div className={styles.markdown}>
+      <Markdown onChange={onChange}>{children}</Markdown>
+    </div>
+  )
 }
