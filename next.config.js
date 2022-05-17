c5d9fa0c8cc940f878b5e32e034107254b6cb371
E. Maric <emaric.dev@gmail.com>
2022-05-17 17:10:56 +0800

    :construction: pwa config update

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const withPWA = require('next-pwa')
 const nextConfig = withPWA({
   pwa: {
     dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
   },
   experimental: {
     outputStandalone: true,
