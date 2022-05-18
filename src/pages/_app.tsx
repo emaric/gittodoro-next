854d5adbad1c9c5980b9fd8480bb9b21a261d589
E. Maric <emaric.dev@gmail.com>
2022-05-18 11:09:29 +0800

    :construction: meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,18 +8,14 @@ import '../styles/globals.css'
 import { GithubAuthProvider } from '@/context/GithubAuthContextProvider'
 import { GittodoroAPIFactoryProvider } from '@/context/GittodoroAPIContextProvider'
 import GittodoroContextProvider from '@/context/gittodoro/GittodoroContextProvider'
+import Meta from '@/components/Meta'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Meta />
       <Head>
-        <title>GIT TODO RO</title>
-        <meta name="theme-color" content="#ffffff" media="(prefers-color-scheme: light)" />
-        <meta name="theme-color" content="#242526" media="(prefers-color-scheme: dark)" />
-        <meta name="background-color" content="#ffffff" media="(prefers-color-scheme: light)" />
-        <meta name="background-color" content="#242526" media="(prefers-color-scheme: dark)" />
-        <link rel='manifest' href='/manifest.json' />
-        <link rel='apple-touch-icon' sizes='180x180' href='/icons/touch-icon-iphone-retina.png' />
+        <title>Gittodoro.</title>
       </Head>
       <Provider store={store}>
         <GithubAuthProvider>
